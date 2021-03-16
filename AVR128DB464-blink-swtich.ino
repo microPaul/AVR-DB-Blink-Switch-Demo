f75f9741d2ce21c48a5d2c965ff1dc534a272930
microPaul <73540066+microPaul@users.noreply.github.com>
2021-03-16 15:55:48 -0400

    Create AVR128DB464-blink-swtich.ino

---
+++ AVR128DB464-blink-swtich.ino
@@ -0,0 +1,52 @@
+///////////////////////////////////////////////////////////
+// AVR-DB Blink-Switch Demo 
+//
+// Intended to run on the Electrodragon.com  AVR128DB64 development board
+// https://www.electrodragon.com/product/avr128db64-mini-develpment-board-avr128/
+// 
+// using Spence Konde's DxCcre for the Arduino IDE.
+// https://github.com/SpenceKonde/DxCore
+//
+// LED active high on PC6
+//
+// Push button switch active low on PC7
+//
+// Operation:  LED blinks at 1000 ms on, 1000 ms off.  Pressing and holding
+// the push button cauess the LED to freeze (remain in current state) while
+// button is in down position.
+//
+///////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////
+// setup 
+//
+// this is the setup, do once, code
+///////////////////////////////////////////////////////////
+void setup(void) {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(PIN_PC6, OUTPUT);  // init PC6 at output for LED
+  pinMode(PIN_PC7, INPUT); // init PC7 as input
+  digitalWrite(PIN_PC7, HIGH);   // enable the pullup on PC7 input pin
+}
+
+///////////////////////////////////////////////////////////
+// loop
+//
+// This is the Super Loop.  In the Arduino world this is really a function (not a loop)
+// that is called repeatedly by the Arduino executive.  As such, any local variables
+// will be lost each time when the function concludes.  If you want to retain local
+// variables then wrap your loop code in a while(1) statement.
+///////////////////////////////////////////////////////////
+void loop(void) {
+  uint32_t timeReference = millis(); // init the time reference
+  while (1) {
+    digitalWrite(PIN_PC6, HIGH);   // activate LED (active high)
+    delay(1000); // keep LED in this state at least this many milliseconds
+    while (!digitalRead(PIN_PC7)) ; // wait for switch to go inactive, thus freezing the LED in prior state
+    
+    digitalWrite(PIN_PC6, LOW);    // disable LED
+    delay(1000); // keep LED in this state at least this many milliseconds
+    while (!digitalRead(PIN_PC7)) ; // wait for switch to go inactive, thus freezing the LED in prior state  
+  }  
+}
+
